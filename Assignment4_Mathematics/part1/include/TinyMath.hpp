497e68cf4b6d6d21ce38d8e397bfdf001cee7e37
phigarcia <phigarcia39@gmail.com>
2023-02-09 10:33:16 -0500

    git being dumb

--- Assignment4_Mathematics/part1/include/TinyMath.hpp
+++ Assignment4_Mathematics/part1/include/TinyMath.hpp
@@ -1,6 +1,12 @@
 #ifndef TINYMATH_HPP
 #define TINYMATH_HPP
 
+#if defined(LINUX) || defined(MINGW)
+#include <SDL2/SDL.h>
+#else // This works for Mac
+#include <SDL.h>
+#endif
+
 #include <cmath>
 #include <ostream>
 
@@ -11,161 +17,209 @@ struct Matrix3D;
 // Vec2D performs vector operations with 3-dimensions
 // The purpose of this class is primarily for 3D graphics
 // applications.
-struct Vec2D{
+struct Vec2D
+{
 
-    // I'm wrapping all of our member variables in a union.
-    // So long as you make sure the byte-size is equivalent, this
-    // allows you to access the data in whatever form is most convenient.
-    // Meaning I can access Vec2D[0] or equivalently Vec2D.x .
-    // I can also pass Vec2D internal array or just individual values if needed conveniently.
-    union{
-        struct {
-            // Note: x,y are a convention
-            // x,y could be position, but also any 2-component value.
-            float x{0};
-            float y{0};
-            float w{0};     // 'w' is a third hidden component that could be used
-                            // to determine if this is being used as a vector or a point
-                            // If w=0 then a vector, if w=1 then a point
-                            // DO not use 'w' for any computation however.
-        };
-        float data[3]; // for convenience if you want to access as 'data' or pass Vec2D as an array 
+  // I'm wrapping all of our member variables in a union.
+  // So long as you make sure the byte-size is equivalent, this
+  // allows you to access the data in whatever form is most convenient.
+  // Meaning I can access Vec2D[0] or equivalently Vec2D.x .
+  // I can also pass Vec2D internal array or just individual values if needed conveniently.
+  union
+  {
+    struct
+    {
+      // Note: x,y are a convention
+      // x,y could be position, but also any 2-component value.
+      float x;
+      float y;
+      float w; // 'w' is a third hidden component that could be used
+               // to determine if this is being used as a vector or a point
+               // If w=0 then a vector, if w=1 then a point
+               // DO not use 'w' for any computation however.
     };
+    float data[3]; // for convenience if you want to access as 'data' or pass Vec2D as an array
+  };
 
-    // Default conostrutcor
-    // 'why default?' https://stackoverflow.com/questions/20828907/the-new-keyword-default-in-c11
-//    Vec2D() = default;
+  // Default conostrutcor
+  // 'why default?' https://stackoverflow.com/questions/20828907/the-new-keyword-default-in-c11
+  //    Vec2D() = default;
 
-    // The "Real" constructor we want to use.
-    // This initializes the values x,y, and the 'w' value
-    Vec2D(float _x=0, float _y=0, float _w=0){
-      // TODO:
-    }
+  // The "Real" constructor we want to use.
+  // This initializes the values x,y, and the 'w' value
+  Vec2D(float _x = 0, float _y = 0, float _w = 0)
+  {
+    x = _x;
+    y = _y;
+    w = _w;
+  }
 
-    // Index operator, allowing us to access the individual
-    // x,y components of our vector.
-    float& operator[](int i){
-        // NOTE: Think about why this works.
-        //       There is no code to change here. 
-      return ((&x)[i]);
-    }
+  // Index operator, allowing us to access the individual
+  // x,y components of our vector.
+  float &operator[](int i)
+  {
+    // NOTE: Think about why this works.
+    //       There is no code to change here.
+    return ((&x)[i]);
+  }
 
-    // Index operator, allowing us to access the individual
-    // x,y components of our vector.
-    const float& operator[](int i) const{
-        // NOTE: Think about why this works.
-        //       There is no code to change here.
-        return ((&x)[i]);
-    }
+  // Index operator, allowing us to access the individual
+  // x,y components of our vector.
+  const float &operator[](int i) const
+  {
+    // NOTE: Think about why this works.
+    //       There is no code to change here.
+    return ((&x)[i]);
+  }
 
+  // General Note
+  // You'll observer that some of the operator overloads appear in this class
+  // as member functions versus some that are free functions.
+  // In general, if you need access to private member variables or the 'this' pointer
+  // then you must use a member function. Otherwise, I recommend to 'free your functions'
+  // and not make them part of the class for maximum use.
+  //
+  // Multiplication Operator
+  // Multiply vector by a uniform-scalar.
 
-    // General Note
-    // You'll observer that some of the operator overloads appear in this class
-    // as member functions versus some that are free functions.
-    // In general, if you need access to private member variables or the 'this' pointer
-    // then you must use a member function. Otherwise, I recommend to 'free your functions'
-    // and not make them part of the class for maximum use.
-    //
-    // Multiplication Operator
-    // Multiply vector by a uniform-scalar.
-    Vec2D& operator *=(float s){
-        // TODO:
-        return (*this);
-    }
+  // TODO questions for ta:
+  // is my real constructor correct - yes
+  // how should i manipulate w within my operations
+  // is there a difference between the += and + operators and what is it
+  Vec2D &operator*=(float s)
+  {
+    x = x * s;
+    y = y * s;
+    return (*this);
+  }
 
-    // Division Operator
-    Vec2D& operator /=(float s){
-        // TODO:
+  // Division Operator
+  Vec2D &operator/=(float s)
+  {
+    x = x / s;
+    y = y / s;
+    return (*this);
+  }
 
-        return (*this);
-    }
+  // Addition operator
+  Vec2D &operator+=(const Vec2D &v)
+  {
+    x = x + v.x;
+    y = y + v.y;
+    return (*this);
+  }
 
-    // Addition operator
-    Vec2D& operator +=(const Vec2D& v){
-        // TODO:
-
-      return (*this);
-    }
-
-    // Subtraction operator
-    Vec2D& operator -=(const Vec2D& v){
-        // TODO:
-
-      return (*this);
-    }
-
+  // Subtraction operator
+  Vec2D &operator-=(const Vec2D &v)
+  {
+    x = x - v.x;
+    y = y - v.y;
+    return (*this);
+  }
 };
 
 // Compute the dot product of a Vec2D
-inline float Dot(const Vec2D& a, const Vec2D& b){
-  // TODO:
-  return 0;
+inline float Dot(const Vec2D &a, const Vec2D &b)
+{
+  float result;
+  result = (a.x * b.x) + (a.y * b.y);
+  return result;
 }
 
 // Test for equality
 // NOTE: Comparing floats is somewhat tricky, meaning that
 //       you will want to take the fabs(lhs.x - lhs.y) < 0.00001
 //       of each component to see if they are 'close enough'
-bool operator==(const Vec2D& lhs, const Vec2D& rhs) {
-    bool result = true;
-    // TODO
-    return result;
+bool operator==(const Vec2D &lhs, const Vec2D &rhs)
+{
+  bool result;
+  if ((fabs(lhs.x - rhs.x) < 0.00001) && (fabs(lhs.y - rhs.y) < 0.00001))
+  {
+    result = true;
+  }
+  else
+  {
+    result = false;
+  }
+  return result;
 }
 
 // Multiplication of a vector by a scalar values
-inline Vec2D operator *(const Vec2D& v, float s){
-  // TODO:
+inline Vec2D operator*(const Vec2D &v, float s)
+{
   Vec2D vec;
+  vec.x = v.x * s;
+  vec.y = v.y * s;
   return vec;
 }
 
 // Division of a vector by a scalar value.
-inline Vec2D operator /(const Vec2D& v, float s){
-  // TODO:
+inline Vec2D operator/(const Vec2D &v, float s)
+{
   Vec2D vec;
+  vec.x = v.x / s;
+  vec.y = v.y / s;
   return vec;
 }
 
 // Negation of a vector
 // Use Case: Sometimes it is handy to apply a force in an opposite direction
-inline Vec2D operator -(const Vec2D& v){
+inline Vec2D operator-(const Vec2D &v)
+{
   // TODO:
   Vec2D vec;
+  vec.x = -v.x;
+  vec.y = -v.y;
   return vec;
 }
 
 // Return the magnitude of a vector
-inline float Magnitude(const Vec2D& v){
-  // TODO:
-  return 0;
+inline float Magnitude(const Vec2D &v)
+{
+  float result;
+  result = sqrt(pow(v.x, 2) + pow(v.y, 2));
+  return result;
 }
 
 // Add two vectors together
-inline Vec2D operator +(const Vec2D& a, const Vec2D& b){
+inline Vec2D operator+(const Vec2D &a, const Vec2D &b)
+{
   // TODO:
+  // if (a.w == 1 && b.w == 1) {
+  //   SDL_Assert(1 == 0);
+  // }
+
   Vec2D vec;
+  vec.x = a.x + b.x;
+  vec.y = a.y + b.y;
   return vec;
 }
 
 // Subtract two vectors
-inline Vec2D operator -(const Vec2D& a, const Vec2D& b){
-  // TODO:
+inline Vec2D operator-(const Vec2D &a, const Vec2D &b)
+{
   Vec2D vec;
-  return vec;
-}
 
-// Vector Projection
-inline Vec2D Project(const Vec2D& a, const Vec2D& b){
-  // TODO:
-  Vec2D vec;
+  vec.x = a.x - b.x;
+  vec.y = a.y - b.y;
   return vec;
 }
 
 // Set a vectors magnitude to 1
 // Note: This is NOT generating a normal vector
-inline Vec2D Normalize(const Vec2D& v){
-  // TODO:
+inline Vec2D Normalize(const Vec2D &v)
+{
   Vec2D vec;
+  vec.x = v.x / Magnitude(v);
+  vec.y = v.y / Magnitude(v);
+  return vec;
+}
+
+// Vector Projection
+inline Vec2D Project(const Vec2D &a, const Vec2D &b)
+{
+  Vec2D vec;
+  vec = Normalize(b) * Dot(a, Normalize(b));
   return vec;
 }
 
@@ -173,93 +227,137 @@ inline Vec2D Normalize(const Vec2D& v){
 // With a 3D vector we would yield another perpendicular cross product
 // For 2D cross product, this will yield a scalar.
 // You should write this to yield a scalar value.
-inline float CrossProduct(const Vec2D& a, const Vec2D& b){
+inline float CrossProduct(const Vec2D &a, const Vec2D &b)
+{
   // TODO:
   float result;
+  result = (a.x * b.y) - (a.y * b.x);
   return result;
 }
 
-// Pretty print a vector 
+// Pretty print a vector
 // std::ostream& could be 'std::cout' for example'
 // This function is primarily used for debugging purposes
-inline void PrettyPrint(std::ostream& os, const Vec2D& a){
-   os << "x  : " << a.x << std::endl; 
-   os << "y  : " << a.y << std::endl; 
-   os << "(w): " << a.w << std::endl; 
+inline void PrettyPrint(std::ostream &os, const Vec2D &a)
+{
+  os << "x  : " << a.x << std::endl;
+  os << "y  : " << a.y << std::endl;
+  os << "(w): " << a.w << std::endl;
 }
 
-
 // Matrix 3D represents 3x3 matrices in Math
-struct Matrix3D{
+struct Matrix3D
+{
 private:
-    float n[3][3];  // Store each value of the matrix
+  float n[3][3]; // Store each value of the matrix
 
 public:
+  // Initializes to a identity matrix by default
+  Matrix3D() // ASK is it okay if i make this 0
+  {
+    n[0][0] = 0;
+    n[0][1] = 0;
+    n[0][2] = 0;
+    n[1][0] = 0;
+    n[1][1] = 0;
+    n[1][2] = 0;
+    n[2][0] = 0;
+    n[2][1] = 0;
+    n[2][2] = 0;
+  }
 
-    // Initializes to a identity matrix by default
-    Matrix3D() {
-        n[0][0] = 1; n[0][1] = 0; n[0][2] = 0;
-        n[1][0] = 0; n[1][1] = 1; n[1][2] = 0;
-        n[2][0] = 0; n[2][1] = 0; n[2][2] = 1;
-    }
+  // TODO: Row or column major order you decide!
+  // Matrix constructor with 9 scalar values.
+  Matrix3D(float n00, float n01, float n02,
+           float n10, float n11, float n12,
+           float n20, float n21, float n22)
+  {
 
-    // TODO: Row or column major order you decide!
-    // Matrix constructor with 9 scalar values.
-    Matrix3D( float n00, float n01, float n02,
-              float n10, float n11, float n12,
-              float n20, float n21, float n22){
+    n[0][0] = n00;
+    n[0][1] = n10;
+    n[0][2] = n20;
+    n[1][0] = n01;
+    n[1][1] = n11;
+    n[1][2] = n21;
+    n[2][0] = n02;
+    n[2][1] = n12;
+    n[2][2] = n22;
+  }
 
-        n[0][0] = n00; n[0][1] = n10; n[0][2] = n20;
-        n[1][0] = n01; n[1][1] = n11; n[1][2] = n21;
-        n[2][0] = n02; n[2][1] = n12; n[2][2] = n22;
-    }
+  // Matrix constructor from three vectors.
+  Matrix3D(const Vec2D &a, const Vec2D &b, const Vec2D &c)
+  {
+    n[0][0] = a.x;
+    n[0][1] = a.y;
+    n[0][2] = a.w;
+    n[1][0] = b.x;
+    n[1][1] = b.y;
+    n[1][2] = b.w;
+    n[2][0] = c.x;
+    n[2][1] = c.y;
+    n[2][2] = c.w;
+  }
 
-    // Matrix constructor from three vectors.
-    Matrix3D(const Vec2D& a, const Vec2D& b, const Vec2D& c){
-      n[0][0] = a.x; n[0][1] = a.y; n[0][2] = a.w;
-      n[1][0] = b.x; n[1][1] = b.y; n[1][2] = b.w;
-      n[2][0] = c.x; n[2][1] = c.y; n[2][2] = c.w;
-    }
+  // Index operator with two dimensions
+  // Example: M(1,1) returns row 1 and column 1 of matrix M.
+  float &operator()(int i, int j)
+  {
+    return (n[j][i]);
+  }
 
-    // Index operator with two dimensions
-    // Example: M(1,1) returns row 1 and column 1 of matrix M.
-    float& operator ()(int i, int j){
-      return (n[j][i]);
-    }
+  // Index operator with two dimensions
+  // Example: M(1,1) returns row 1 and column 1 of matrix M.
+  const float &operator()(int i, int j) const
+  {
+    return (n[j][i]);
+  }
 
-    // Index operator with two dimensions
-    // Example: M(1,1) returns row 1 and column 1 of matrix M.
-    const float& operator ()(int i, int j) const{
-      return (n[j][i]);
-    }
+  // Return a row from a matrix as a vector.
+  Vec2D &operator[](int j)
+  {
+    return (*reinterpret_cast<Vec2D *>(n[j]));
+  }
 
-    // Return a row from a matrix as a vector.
-    Vec2D& operator [](int j){
-      return (*reinterpret_cast<Vec2D *>(n[j]));
-    }
-
-    // Return a row from a matrix as a vector.
-    const Vec2D& operator [](int j) const{
-      return (*reinterpret_cast<const Vec2D *>(n[j]));
-    }
-
+  // Return a row from a matrix as a vector.
+  const Vec2D &operator[](int j) const
+  {
+    return (*reinterpret_cast<const Vec2D *>(n[j]));
+  }
 };
 
 // Matrix Multiplication
-Matrix3D operator *(const Matrix3D& A, const Matrix3D& B){
-  // TODO:
+Matrix3D operator*(const Matrix3D &A, const Matrix3D &B)
+{
   Matrix3D result;
-    
+  for (int i = 0; i < 3; i++)
+  {
+    for (int j = 0; j < 3; j++)
+    {
+      for (int k = 0; k < 3; k++)
+      {
+        result[i][j] += A[i][k] * B[k][j];
+      }
+    }
+  }
+
   return result;
 }
 
 // Matrix multiply by a vector
-Vec2D operator *(const Matrix3D& M, const Vec2D& v){
+Vec2D operator*(const Matrix3D &M, const Vec2D &v)
+{
   // TODO:
   Vec2D vec;
-    
+  Matrix3D fakeMatrix;
+  Matrix3D resultMatrix;
+  fakeMatrix[0][0] = v.x;
+  fakeMatrix[1][1] = v.y;
+  fakeMatrix[2][2] = 1;
+
+  resultMatrix = M * fakeMatrix;
+  vec.x = resultMatrix(0,0) / resultMatrix(2,2);
+  vec.y = resultMatrix(1,1) / resultMatrix(2,2);
   return vec;
 }
-
 
 #endif
--- Assignment4_Mathematics/part1/tests/tests.cpp
+++ Assignment4_Mathematics/part1/tests/tests.cpp
@@ -4,13 +4,14 @@
 #include <iostream>
 
 // Add a 1-vector with a 0-vector.
-bool unitTest1(){
-    Vec2D a(1,1);
-    Vec2D b(0,0);
+bool unitTest1()
+{
+    Vec2D a(1, 1);
+    Vec2D b(0, 0);
     Vec2D result = a + b;
 
-
-    if(result.x == 1 && result.y == 1){
+    if (result.x == 1 && result.y == 1)
+    {
         return true;
     }
     // You generally should not be printing out the
@@ -24,53 +25,177 @@ bool unitTest1(){
 }
 
 // TODO
-bool unitTest2(){
+// multiply
+bool unitTest2()
+{
+    Vec2D a(1, 2);
+    Vec2D result = a * 2;
+    if (result.x == 2 && result.y == 4)
+    {
+        return true;
+    }
     return false;
 }
 // TODO
-bool unitTest3(){
+// divide
+bool unitTest3()
+{
+    Vec2D a(1, 2);
+    Vec2D result = a / 2;
+    if (result.x == .5f && result.y == 1)
+    {
+        return true;
+    }
     return false;
 }
 // TODO
-bool unitTest4(){
+// add
+bool unitTest4()
+{
+    Vec2D a(1, 2);
+    Vec2D b(3, 4);
+    Vec2D result = a + b;
+    if (result.x == 4 && result.y == 6)
+    {
+        return true;
+    }
     return false;
 }
 // TODO
-bool unitTest5(){
+// subtract
+bool unitTest5()
+{
+    std::cout << "Subtract: " << std::endl;
+    Vec2D a(1, 2);
+    Vec2D b(3, 4);
+    Vec2D result = a - b;
+    if (result.x == -2 && result.y == -2)
+    {
+        return true;
+    }
     return false;
 }
 // TODO
-bool unitTest6(){
+// normalize
+bool unitTest6()
+{
+    Vec2D a(3, 4);
+    Vec2D result = Normalize(a);
+    std::cout << result.x << ", " << result.y << std::endl;
+    if (result.x == (.6f) && result.y == (.8f))
+    {
+        return true;
+    }
     return false;
 }
 // TODO
-bool unitTest7(){
+// magnitude
+bool unitTest7()
+{
+    Vec2D a(3, 4);
+    float result = Magnitude(a);
+    if (result == 5)
+    {
+        return true;
+    }
     return false;
 }
 // TODO
-bool unitTest8(){
+// 3x3 by 3x3
+bool unitTest8()
+{
+    Matrix3D a(0, 1, 2,
+               3, 4, 5,
+               6, 7, 8);
+    Matrix3D b(1, 1, 1,
+               1, 1, 1,
+               1, 1, 1);
+    Matrix3D result = a * b;
+
+    if (result(0, 0) == 9 && result(0, 1) == 12 && result(0, 2) == 15 &&
+        result(1, 0) == 9 && result(1, 1) == 12 && result(1, 2) == 15 &&
+        result(2, 0) == 9 && result(2, 1) == 12 && result(2, 2) == 15)
+    {
+        return true;
+    }
     return false;
 }
 // TODO
-bool unitTest9(){
+// dot product
+bool unitTest9()
+{
+    std::cout << "dot: " << std::endl;
+    Vec2D a(4, 5);
+    Vec2D b(-5, 2);
+    float result = Dot(a, b);
+    if (result == -10)
+    {
+        return true;
+    }
     return false;
 }
 // TODO
-bool unitTest10(){
+// project
+bool unitTest10()
+{
+    std::cout << "Project: " << std::endl;
+    Vec2D a(-1, 5);
+    Vec2D b(2, 4);
+    Vec2D result = Project(a, b);
+    std::cout << result.x << ", " << result.y << std::endl;
+    if (result.x == 1.8f && result.y == 3.6f)
+    {
+        return true;
+    }
     return false;
 }
 // TODO
-bool unitTest11(){
+// negation
+bool unitTest11()
+{
+    Vec2D a(1, -2);
+    Vec2D result = -a;
+    if (result.x == -1 && result.y == 2)
+    {
+        return true;
+    }
     return false;
 }
 // TODO
-bool unitTest12(){
+// cross product
+bool unitTest12()
+{
+    std::cout << "Cross Product: " << std::endl;
+    Vec2D a(5, 5);
+    Vec2D b(3, 2);
+    float result = CrossProduct(a, b);
+    if (result == -5)
+    {
+        return true;
+    }
     return false;
 }
 
+// matrix x vector
+bool unitTest13()
+{
+    std::cout << "Matrix x Vector : " << std::endl;
+    Vec2D a(3, 2);
+    Matrix3D M(1, 0, 6,
+               0, 1, 5,
+               0, 0, 1);
+    Vec2D result = M * a;
+        std::cout << result.x << ", " << result.y << std::endl;
+
+    if (result.x = 9 && result.y == 7)
+    {
+        return true;
+    }
+    return false;
+}
 
 // TODO: Add more tests here at your discretion
-bool (*unitTests[])(void)={
+bool (*unitTests[])(void) = {
     unitTest1,
     unitTest2,
     unitTest3,
@@ -82,35 +207,40 @@ bool (*unitTests[])(void)={
     unitTest9,
     unitTest10,
     unitTest11,
-    unitTest12,	
-    nullptr
-};
-
+    unitTest12,
+    unitTest13,
+    nullptr};
 
 // ====================================================
 // ================== Program Entry ===================
 // ====================================================
-int main(){
+int main(int argv, char *args[])
+{
     unsigned int testsPassed = 0;
     // List of Unit Tests to test your data structure
-    int counter =0;
+    int counter = 0;
 
     // Iterate through the array of unitTest functions until the
     // nullptr is found indicating the end of the unitTest array.
-    while( unitTests[counter] != nullptr ){
+    while (unitTests[counter] != nullptr)
+    {
         std::cout << "========unitTest " << counter << "========\n";
 
-        if( true == unitTests[counter]() ){
+        if (true == unitTests[counter]())
+        {
             std::cout << "passed test\n";
-            testsPassed++;	
-        }else{
+            testsPassed++;
+        }
+        else
+        {
             std::cout << "failed test, missing functionality, or incorrect test\n";
         }
 
         ++counter;
     }
 
-    std::cout << "\n\n" << testsPassed << " of " << counter << " tests passed\n";
+    std::cout << "\n\n"
+              << testsPassed << " of " << counter << " tests passed\n";
 
     return 0;
 }
